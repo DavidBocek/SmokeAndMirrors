db66de6faf6ce71cbdca5a9e5f8ae2410e5dd5d4
DavidBocek <dmbocek@gmail.com>
2014-01-24 22:51:06 -0500

    pushing initial moveme nt

---
+++ SmokeAndMirrors/Assets/Scripts/LookY.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookY : MonoBehaviour {
+	public float sensitivityY;
+	public bool inverted;
+	
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = false;
+	}
+	
+	void LateUpdate () {
+		//adjust vertical rotation
+		float curRotX = transform.eulerAngles.x;
+		float xAngleAdjust = inverted ? Input.GetAxis("Mouse Y") : -Input.GetAxis ("Mouse Y");
+		curRotX += xAngleAdjust * sensitivityY;
+		if (curRotX < 275f && curRotX > 180f){curRotX = 275.0f;}
+		else if (curRotX > 85f && curRotX < 180f){curRotX = 85.0f;}
+		//input new rotation
+		transform.localEulerAngles = new Vector3(curRotX,0,0);
+	}
+}
---
+++ SmokeAndMirrors/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float walkSpeed;
+	public float runSpeed;
+	public float strafeSpeed;
+	public float gravityAcceleration;
+	public float mouseXSensitivity;
+	public float maxFallSpeed;
+	
+	private Vector3 velocity;
+	private CharacterController controller;
+
+	private float dt;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		velocity = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dt = Time.smoothDeltaTime;
+		UpdateGravity();
+		UpdatePositionAndRotation();
+	}
+
+	void UpdateGravity(){
+		if (!controller.isGrounded){
+			velocity.y -= gravityAcceleration * dt;
+			velocity.y = Mathf.Max (velocity.y,maxFallSpeed);
+		}
+	}
+
+	void UpdatePositionAndRotation(){
+		velocity.z = Input.GetAxis ("Vertical") * walkSpeed;
+		velocity.x = Input.GetAxis ("Horizontal") * strafeSpeed;
+
+		Vector3 rotation = transform.localEulerAngles;
+		rotation.y += Input.GetAxis("Mouse X")*mouseXSensitivity;
+		velocity = transform.TransformDirection(velocity);
+
+		transform.localEulerAngles = rotation;
+		controller.Move(velocity*dt);
+	}
+}
