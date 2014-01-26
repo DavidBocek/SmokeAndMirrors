957078cc153e1046f5b64f5c39e31c0dc34edbcb
DavidBocek <dmbocek@gmail.com>
2014-01-26 03:09:10 -0500

    rightside up room barrels and flaghlight

---
+++ SmokeAndMirrors/Assets/LerpLightIntensity.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpLightIntensity : MonoBehaviour {
+
+	public float stepPerFrame;	//controls lerp speed
+	public float lowIntensity;
+	public float highIntensity;
+
+	private Light light;
+	private float t = 0;
+	private bool increasing = true;
+	private bool decreasing = false;
+	
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		light.intensity = Mathf.Lerp(lowIntensity,highIntensity,t);
+		if (increasing){
+			if (t>=highIntensity){
+				increasing = false;
+				decreasing = true;
+				t = highIntensity;
+			}
+			t += stepPerFrame;
+		} else if (decreasing) {
+			if (t<=lowIntensity){
+				increasing = true;
+				decreasing = false;
+				t = lowIntensity;
+			}
+			t -= stepPerFrame;
+		}
+	}
+}
