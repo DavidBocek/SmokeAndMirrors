556023cfb51fd8c525fb4e91f9d7fcd86fe41e4d
DavidBocek <dmbocek@gmail.com>
2014-01-25 23:07:19 -0500

    it worked

---
+++ SmokeAndMirrors/Assets/PlayBangingNoiseLR2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayBangingNoiseLR2 : MonoBehaviour {
+
+	public AudioSource bangingSoundSource;
+
+	// Use this for initialization
+	void Start () {
+		Messenger.AddListener<GameObject>("TriggerHit",HandleTrigger);
+	}
+	
+	void HandleTrigger(GameObject whatWasHit){
+		if (gameObject == whatWasHit){
+			bangingSoundSource.Play();
+		}
+	}
+}
